01f19538ea8bf474a0b3a2db73764addd67bca80
eric-antonio <ericantoniodev@gmail.com>
2024-02-16 20:29:36 +0200

    Hero Section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./global/globalStyle";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/pricing" element={<Pricing />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Hero/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Container, MainHeading } from "../../global/globalStyle";
+import {
+  HeroVideo,
+  HeroSection,
+  HeroText,
+  ButtonWrapper,
+  HeroButton,
+} from "./styles";
+
+function Hero() {
+  return (
+    <HeroSection>
+      <HeroVideo></HeroVideo>
+      <Container>
+        <MainHeading> Your data is secure with us!</MainHeading>
+        <HeroText>
+          We provide the best security systems for the clientes all over the
+          world
+        </HeroText>
+        <ButtonWrapper>
+          <Link to="signup">
+            <Button>Get Started</Button>
+          </Link>
+          <HeroButton>Finde More</HeroButton>
+        </ButtonWrapper>
+      </Container>
+    </HeroSection>
+  );
+}
+
+export default Hero;
