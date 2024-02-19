be92bf5d3033a3369e4b45144bff68a805162c8e
eric-antonio <ericantoniodev@gmail.com>
2024-02-19 09:14:20 +0200

    Contentent fineshed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import React from "react";
+import Hero from "../components/Hero";
+import Features from "../components/Features/index";
+import { Content } from "../components/Content";
+import { heroOne, heroThree, heroTwo } from "../data/HeroData";
 
 function Home() {
   return (
-    <Hero/>
-  )
+    <>
+      <Hero />
+      <Features />
+      <Content {...heroOne}/>
+      <Content {...heroTwo}/>
+      <Content {...heroThree}/>
+      
+    </>
+  );
 }
 
-export default Home
+export default Home;
